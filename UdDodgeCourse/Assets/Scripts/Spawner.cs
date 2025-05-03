7338bcd8cc70b671924a071a01c5303605e52674
ThallasTV <josh-eatwell@hotmail.com>
2025-05-03 17:20:23 +0100

    Updates
    Destroy spheres to avoid screen clutter and performance reduction

--- UdDodgeCourse/Assets/Scripts/Spawner.cs
+++ UdDodgeCourse/Assets/Scripts/Spawner.cs
@@ -8,6 +8,8 @@ public class Spawner : MonoBehaviour
     [SerializeField] private float spawnInterval = 2f; // Time interval between spawns
     [SerializeField] private float spawnHeight = 5f; // Height above the ramp to spawn spheres
     [SerializeField] private float spawnRangeX = 10f; // Range along the X-axis for spawning
+    [SerializeField] private float spawnRangeXTwo = 10f; // Range along the X-axis for spawning
+    [SerializeField] private float destroySphere = 10f; // Time before destroying the sphere
 
     private float timer;
 
@@ -26,18 +28,25 @@ public class Spawner : MonoBehaviour
     {
         // Calculate a random X position within the spawn range
         float randomX = Random.Range(-spawnRangeX, spawnRangeX);
+        // Calculate a random X position within the spawn range
+        float randomXTwo = Random.Range(-spawnRangeXTwo, spawnRangeXTwo);
 
         // Determine the spawn position above the ramp
         Vector3 spawnPosition = new Vector3(rampTransform.position.x + randomX, rampTransform.position.y + spawnHeight,
                                             rampTransform.position.z
         );
 
-        Vector3 spawnPositionTwo = new Vector3(rampTransformTwo.position.x + randomX, rampTransformTwo.position.y + spawnHeight,
+        Vector3 spawnPositionTwo = new Vector3(rampTransformTwo.position.x + randomXTwo, rampTransformTwo.position.y + spawnHeight,
                                                 rampTransformTwo.position.z
         );
 
         // Instantiate the sphere at the calculated position
-        Instantiate(spherePrefab, spawnPosition, Quaternion.identity);
-        Instantiate(spherePrefab, spawnPositionTwo, Quaternion.identity);
+        GameObject sphereOne = Instantiate(spherePrefab, spawnPosition, Quaternion.identity);
+        GameObject sphereTwo = Instantiate(spherePrefab, spawnPositionTwo, Quaternion.identity);
+
+        Destroy(sphereOne, destroySphere); // Destroy the sphere after a certain time
+        Destroy(sphereTwo, destroySphere); // Destroy the sphere after a certain time
     }
+
+    
 }
